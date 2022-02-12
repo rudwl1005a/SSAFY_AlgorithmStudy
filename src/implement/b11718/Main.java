900e5e33fef1229ac4c2218062a745400e11f36a
Min kyungdae <rudwl1005@naver.com>
2022-02-12 13:37:20 +0900

    extra study

---
+++ src/implement/b11718/Main.java
@@ -0,0 +1,21 @@
+package implement.b11718;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 백준 브론즈3 ( https://www.acmicpc.net/problem/11718 )
+ */
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s = "";
+		while((s = br.readLine()) != null) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
