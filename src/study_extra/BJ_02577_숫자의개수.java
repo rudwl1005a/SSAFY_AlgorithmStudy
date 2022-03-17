f52ec55c96cc6aae60c65b896622b9599d5c481e
Min kyungdae <rudwl1005@naver.com>
2022-03-18 02:20:08 +0900

    extra study

---
+++ src/study_extra/BJ_02577_숫자의개수.java
@@ -0,0 +1,38 @@
+package study_extra;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 백준 B2 ( https://www.acmicpc.net/problem/2577 )
+ */
+public class BJ_02577_숫자의개수 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n1 = Integer.parseInt(br.readLine());
+		int n2 = Integer.parseInt(br.readLine());
+		int n3 = Integer.parseInt(br.readLine());
+		
+		int num = n1 * n2 * n3;
+		
+		int[] numArray = new int[10];
+		
+		while(true) {
+			if(num <= 0) {
+				break;
+			}
+			int n = num % 10;
+			
+			numArray[n]++;
+			
+			num /= 10;
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(numArray[i]);
+		}
+	}
+
+}
