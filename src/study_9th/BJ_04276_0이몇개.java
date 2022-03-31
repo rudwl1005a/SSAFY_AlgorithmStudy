5e8872a8559ac7b19df892c9378b5a61526c52cf
Min kyungdae <rudwl1005@naver.com>
2022-03-31 23:36:18 +0900

    9th study

---
+++ src/study_9th/BJ_04276_0이몇개.java
@@ -0,0 +1,52 @@
+package study_9th;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 백준 G3 ( https://www.acmicpc.net/problem/4276 )
+ */
+public class BJ_04276_0이몇개 {
+
+	static long to, from;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			to = Long.parseLong(st.nextToken());
+			from = Long.parseLong(st.nextToken());
+			if (to == -1 && from == -1) {
+				break;
+			}
+
+			System.out.println(Math.abs(count0(to - 1) - count0(from)));
+		}
+	}
+
+	private static long count0(long n) {
+		if (n < 0) { // 0부터 시작하는 예외를 위해서
+			return 0;
+		}
+
+		long result = 0;
+		long ten = 1;
+		while (n / ten != 0) {
+			long next = n / (ten * 10);
+			long current = (n % (ten * 10)) / ten;
+			long prev = n % ten;
+
+			if (current != 0) {
+				result += next * ten;
+			} else {
+				result += (next - 1) * ten + (prev + 1);
+			}
+
+			ten *= 10;
+		}
+		return result + 1;
+	}
+
+}
