e1d79e6ef3f0dea232f1d804ba172c41af3fed88
Min kyungdae <rudwl1005@naver.com>
2022-04-17 19:19:40 +0900

    extra study

---
+++ src/study_extra/BJ_02920_음계.java
@@ -0,0 +1,42 @@
+package study_extra;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 백준 B2 ( https://www.acmicpc.net/problem/2920 )
+ */
+public class BJ_02920_음계 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[8];
+		for (int i = 0; i < 8; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		if (arr[0] == 1) {
+			boolean isAscending = true;
+			for (int i = 1; i < 8; i++) {
+				if (arr[i] != (i + 1)) {
+					isAscending = false;
+				}
+			}
+			System.out.println(isAscending ? "ascending" : "mixed");
+		} else if (arr[0] == 8) {
+			boolean isDecending = true;
+			for (int i = 1; i < arr.length; i++) {
+				if (arr[i] != (8 - i)) {
+					isDecending = false;
+				}
+			}
+			System.out.println(isDecending ? "descending" : "mixed");
+		} else {
+			System.out.println("mixed");
+		}
+	}
+
+}
