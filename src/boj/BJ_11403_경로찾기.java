2279eaddfed4340f20977fb3c68579ad53fd7be2
Min kyungdae <rudwl1005@naver.com>
2022-09-22 23:03:16 +0900

    [백준 S1] 11403-경로 찾기

---
+++ src/boj/BJ_11403_경로찾기.java
@@ -0,0 +1,52 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 백준 S1 ( https://www.acmicpc.net/problem/11403 )
+ */
+public class BJ_11403_경로찾기 {
+
+	static int N, map[][];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		// i에서 j까지 갈 수 있는가?
+		// i에서 k로 가고, k에서 j로 갈 수 있는가?
+		// 위에 2개의 질문은 동일함.
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					// 단순히 갈 수 있는 경로가 있는지만 체크함.
+					if (map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
