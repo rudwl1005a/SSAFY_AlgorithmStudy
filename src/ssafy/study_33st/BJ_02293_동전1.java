c562c9a6b7021d7b873274c4cd7437a172116d31
Min kyungdae <rudwl1005@naver.com>
2022-12-29 23:19:46 +0900

    [백준 G5] 2293-동전1

---
+++ src/ssafy/study_33st/BJ_02293_동전1.java
@@ -0,0 +1,42 @@
+package ssafy.study_33st;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 백준 G5 ( https://www.acmicpc.net/problem/2293 )
+ */
+public class BJ_02293_동전1 {
+
+	static int N, K, coin[], dp[];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		coin = new int[N];
+		dp = new int[K + 1];
+
+		// 코인 종류 저장
+		for (int i = 0; i < N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+
+		// 저장된 코인 종류로 만들 수 있는 K이하의 경우의 수 저장
+		dp[0] = 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = coin[i]; j <= K; j++) {
+				dp[j] += dp[j - coin[i]];
+			}
+		}
+
+		// 저장된 코인 종류로 만들 수 있는 K의 경우의 수 출력
+		System.out.println(dp[K]);
+
+	}
+
+}
