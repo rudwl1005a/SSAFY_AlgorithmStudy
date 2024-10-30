6b743f1d4f0f5e3d546d597347627ec0c2422804
Min kyungdae <rudwl1005@naver.com>
2024-10-30 22:58:19 +0900

    [백준 S3] 9095 - 1,2,3 더하기

--- src/boj/BJ_09095_123더하기.java
+++ src/boj/BJ_09095_123더하기.java
@@ -1,16 +1,15 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /*
  * 백준 S3 ( https://www.acmicpc.net/problem/9095 )
  */
 public class BJ_09095_123더하기 {
 
-	static int N, dp[];
+	static int T, N, dp[];
 
-	public static void main(String[] args) throws Exception {
+	public static void main_old(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		dp = new int[11];
@@ -32,4 +31,30 @@ public class BJ_09095_123더하기 {
 		System.out.println(sb);
 	}
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		dp = new int[12];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for(int i = 4; i < 12; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		T = Integer.parseInt(br.readLine());
+		for(int t = 0; t < T; t++) {
+			N = Integer.parseInt(br.readLine());
+			sb.append(dp[N] + "\n");
+		}
+		sb.delete(sb.length() - 1, sb.length());
+		bw.write(sb.toString());
+
+		bw.flush();
+		bw.close();
+	}
 }
