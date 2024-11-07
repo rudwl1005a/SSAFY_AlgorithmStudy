682c91357f82de709d07ac09b7333044a0dbb745
Min kyungdae <rudwl1005@naver.com>
2024-11-07 22:46:15 +0900

    [백준 S2] 11053 - 가장 긴 증가하는 부분 수열

--- src/boj/BJ_11053_가장긴증가하는부분수열.java
+++ src/boj/BJ_11053_가장긴증가하는부분수열.java
@@ -25,10 +25,13 @@ public class BJ_11053_가장긴증가하는부분수열 {
 
 		dp[0] = 1;
 		for (int i = 1; i < N; i++) {
+			// 모든 수열의 길이는 최소 1
 			dp[i] = 1;
+			// i이전의 원소들 탐색
 			for (int j = 0; j < i; j++) {
+				// j번째 원소가 i번째 원소보다 작으면서 i번째 dp가 j번째 dp보다 작거나 같은경우
 				if (arr[j] < arr[i] && dp[i] <= dp[j]) {
-					dp[i] = dp[j] + 1;
+					dp[i] = dp[j] + 1; // 수열 증가하여 dp에 저장
 				}
 			}
 		}
