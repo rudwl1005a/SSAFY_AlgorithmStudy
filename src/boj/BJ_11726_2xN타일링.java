b4898e641d15d5cacf17bbf1df6bc9aacf6e2e37
Min kyungdae <rudwl1005@naver.com>
2024-11-08 23:44:07 +0900

    [백준 S3] 11726 - 2×n 타일링

--- src/boj/BJ_11726_2xN타일링.java
+++ src/boj/BJ_11726_2xN타일링.java
@@ -1,7 +1,6 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /*
  * 백준 S3 ( https://www.acmicpc.net/problem/11726 )
@@ -10,7 +9,7 @@ public class BJ_11726_2xN타일링 {
 
 	static int N, dp[];
 
-	public static void main(String[] args) throws Exception {
+	public static void main_old(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
@@ -27,4 +26,23 @@ public class BJ_11726_2xN타일링 {
 		System.out.println(dp[N]);
 	}
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
+
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for(int i = 2; i <= N; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+		}
+
+		bw.write(dp[N] + "\n");
+
+		bw.flush();
+		bw.close();
+	}
 }
