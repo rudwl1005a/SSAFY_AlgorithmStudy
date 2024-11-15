231cd4741f6ba934d49d8c9d739f6d1531852680
Min kyungdae <rudwl1005@naver.com>
2024-11-15 23:05:00 +0900

    [백준 S3] 2193 - 이친수

--- src/boj/BJ_02193_이친수.java
+++ src/boj/BJ_02193_이친수.java
@@ -1,7 +1,6 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /*
  * 백준 S3 ( https://www.acmicpc.net/problem/2193 )
@@ -11,7 +10,7 @@ public class BJ_02193_이친수 {
 	static int N;
 	static long dp[];
 
-	public static void main(String[] args) throws Exception {
+	public static void main_old(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
@@ -27,4 +26,24 @@ public class BJ_02193_이친수 {
 		System.out.println(dp[N]);
 	}
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+		dp = new long[N + 1];
+
+		dp[0] = 0;
+		dp[1] = 1;
+
+		// 피보나치 수열과 같다
+		for(int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		bw.write(dp[N] + "");
+
+		bw.flush();
+		bw.close();
+	}
 }
